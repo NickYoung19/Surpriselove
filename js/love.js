5fdaabf0522f10afaa924a5dcc01a529e02c9da1
Nick.Peng <50699427+NickyangPeng@users.noreply.github.com>
2019-05-19 22:37:57 +0800

    Update love.js

--- js/love.js
+++ js/love.js
@@ -1,6 +1,11 @@
 ﻿(function(window){
-     $('html').on('touchstart',function(){
-         audio.play();
+     // 创建页面监听，等待微信端页面加载完毕 触发音频播放
+     document.addEventListener('WeixinJSBridgeReady', function() {
+          document.getElementById('audios').play()
+     })
+     // 创建触摸监听，当浏览器打开页面时，触摸屏幕触发事件，进行音频播放
+     document.addEventListener('touchstart', function() {
+          document.getElementById('audios').play()
      })
 
     function random(min, max) {
